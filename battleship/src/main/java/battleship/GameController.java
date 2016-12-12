827847880acb4d5f925db6bf9fd0a6685236fbff
hmccabe <hmccabe@csumb.edu>
2016-12-11 22:53:00 -0800

    added automatic ship placement

--- battleship/src/main/java/battleship/GameController.java
+++ battleship/src/main/java/battleship/GameController.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Hashtable;
+import java.util.Random;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -66,22 +67,22 @@ public class GameController extends HttpServlet {
 	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		// Create a new game
 		UUID newGameId = UUID.randomUUID();
 		GameModel newGame = new GameModel(1);
 
-		/*
-		 * The 1990 Milton Bradley version of the rules specify the following
-		 * ships:
-		 */
+		/* note the 1990 Milton Bradley version of the rules specifies the following ships */
 
-		Hashtable<String, Ship> p1 = new Hashtable<String, Ship>();
-
+		// Create player 1's ships
+		Hashtable<String, Ship> p1 = new Hashtable<String, Ship>();		
 		p1.put("Carrier", new Ship("Carrier", 5));
 		p1.put("Battleship", new Ship("Battleship", 4));
 		p1.put("Cruiser", new Ship("Cruiser", 3));
 		p1.put("Submarine", new Ship("Submarine", 3));
 		p1.put("Destroyer", new Ship("Destroyer", 2));
 
+		// Create player 2's ships
 		Hashtable<String, Ship> p2 = new Hashtable<String, Ship>();
 		p2.put("Carrier", new Ship("Carrier", 5));
 		p2.put("Battleship", new Ship("Battleship", 4));
@@ -89,22 +90,26 @@ public class GameController extends HttpServlet {
 		p2.put("Submarine", new Ship("Submarine", 3));
 		p2.put("Destroyer", new Ship("Destroyer", 2));
 
+		// Add the ship tables to the game object
 		newGame.setPlayer1Ships(p1);
 		newGame.setPlayer2Ships(p2);
+		
+		// Automatically select ship locations
+		placeShips(newGame,1);
+		placeShips(newGame,2);
 
+		// Store the game in the games Hashtable
 		games.put(newGameId, newGame);
 		
 		//Test returning ship
-
 		String playerShips = "{\"Ships\": [{\"ship\": \"Submarine\",\"hits\": \"1\",\"location\": [{\"x\": 1,\"y\": 1,\"hit\": false}, {\"x\": 1,\"y\": 2,\"hit\": true}, {\"x\": 1,\"y\": 3,\"hit\": false}]},{\"ship\":\"Destroyer\",\"hits\": \"1\",\"location\": [{\"x\": 3,\"y\": 1,\"hit\": false}, {\"x\": 4,\"y\": 1,\"hit\": true}]}]}";
-	   String opponentShips = "{\"Ships\": [{\"ship\": \"Submarine\",\"hits\": \"1\",\"location\": [{\"x\": 1,\"y\": 1,\"hit\": false}, {\"x\": 1,\"y\": 2,\"hit\": true}, {\"x\": 1,\"y\": 3,\"hit\": false}]},{\"ship\":\"Destroyer\",\"hits\": \"1\",\"location\": [{\"x\": 3,\"y\": 1,\"hit\": false}, {\"x\": 4,\"y\": 1,\"hit\": true}]}]}";
+	    String opponentShips = "{\"Ships\": [{\"ship\": \"Submarine\",\"hits\": \"1\",\"location\": [{\"x\": 1,\"y\": 1,\"hit\": false}, {\"x\": 1,\"y\": 2,\"hit\": true}, {\"x\": 1,\"y\": 3,\"hit\": false}]},{\"ship\":\"Destroyer\",\"hits\": \"1\",\"location\": [{\"x\": 3,\"y\": 1,\"hit\": false}, {\"x\": 4,\"y\": 1,\"hit\": true}]}]}";
 		JSONObject json = new JSONObject();
 		json.put("game_id", newGameId.toString());
 		json.put("player_fleet", playerShips);
 		json.put("opponent_fleet", opponentShips);
 		
 		response.addHeader("Content-Type", "application/json");
-
 		response.getWriter().append(json.toString());
 	}
 
@@ -212,6 +217,87 @@ public class GameController extends HttpServlet {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
+	
+	private boolean placeShips(GameModel game,int playerNumber) {
+		// Get the player's table of ships and game grid
+		Hashtable<String,Ship> ships;
+		if (playerNumber == 1) {
+			ships = game.getPlayer1Ships();
+		} else if (playerNumber == 2) {
+			ships = game.getPlayer2Ships();
+		} else {
+			// Return false if an invalid player number is provided
+			return false;
+		}
+
+		// Iterate through the Hashtable of ships to place them
+		Set<String> keys = ships.keySet();
+		for(String key : keys){
+			Ship currentShip = ships.get(key);
+
+			int length = currentShip.getHits();
+
+			// Select a random number 0-1 to determine orientation of ship
+			Random rand = new Random();
+			int orientation = rand.nextInt(2);
+
+			// Loop picking random locations until we find one that is not already occupied
+			while (true) {
+				Point[] currentPoints = new Point[length];
+
+				if (orientation == 0) {
+					// Horizontal orientation
+					// Select a random starting point
+					int x = rand.nextInt(10 - length); // subtract the length so it won't overflow the board
+					int y = rand.nextInt(10);					
+					
+					// Create points
+					for (int i = 0; i < length; i++) {
+						currentPoints[i] = new Point(x+i,y,false);
+					}
+				} else {
+					// Vertical orientation
+					// Select a random starting point
+					int x = rand.nextInt(10);
+					int y = rand.nextInt(10 - length); // subtract the length so it won't overflow the board
+					
+					// Create points
+					for (int i = 0; i < length; i++) {
+						currentPoints[i] = new Point(x,y+i,false);
+					}
+				}
+
+				// Check if there is already a ship in this location
+				boolean shipOverlaps = false;
+				for (String k : keys) {
+					if (shipOverlaps) break; // stop checking if we already know one overlaps
+					
+					// Must check each point of each of the other ships
+					Ship otherShip = ships.get(k);			
+					if (otherShip.getLocation() != null) {
+						// Get the points in the other ship
+						Point[] otherPoints = otherShip.getLocation().getPoints();
+						
+						// Check each point
+						for (Point cp : currentPoints) {
+							for (Point op : otherPoints) {
+								if (cp.getX() == op.getX() && cp.getY() == op.getY()) {
+									shipOverlaps = true;
+								}
+							}
+						}
+					}	
+				}
+
+				// If no ships overlap, set the location for this ship
+				// Otherwise, while loop will restart
+				if (shipOverlaps == false) {
+					currentShip.setLocation(new Location(currentPoints));
+				}
+			}
+	}
+	return true;
+}
 
 	private class RestRequest {
 		// Accommodate two requests, one for all resources, another for a
