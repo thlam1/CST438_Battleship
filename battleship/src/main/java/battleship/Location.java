bb72dcbd6f71227a0a8196fe7e77851f37ec295a
hmccabe <hmccabe@csumb.edu>
2016-12-12 17:04:05 -0800

    added method to get individual Point from Location so we can update the state

--- battleship/src/main/java/battleship/Location.java
+++ battleship/src/main/java/battleship/Location.java
@@ -18,4 +18,13 @@ public class Location {
 	public void setPoints(Point[] points) {
 		this.points = points;
 	}
+	
+	public Point getSinglePoint(int x, int y) {
+		for (Point p : points) {
+			if (p.getX() == x && p.getY() == y) {
+				return p;
+			}
+		}
+		return null;
+	}
 }
